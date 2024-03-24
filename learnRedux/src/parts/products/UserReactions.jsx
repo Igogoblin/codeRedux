d8ef0fbced03a432a30f442a0002b27661fc9c9d
Ihar Skavysh <igogoblin@gmail.com>
2024-03-24 21:49:27 +0300

    fix: change button by user reaction

--- learnRedux/src/parts/products/UserReactions.jsx
+++ learnRedux/src/parts/products/UserReactions.jsx
@@ -1,21 +1,26 @@
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { reactionClicked } from "./ProductsSlice";
 const reactionObj = {
   good: "+",
   soso: "+/-",
   bad: "-",
 };
-const UserReaction = ({ product }) => {
+const UserReactions = ({ product }) => {
   const dispatch = useDispatch();
-  const userReaction = Object.entries(reactionObj).map([name,image])=>{
-    return(
-        <button key={name} type="button" className="reaction-button"
-        onClick={dispatch(reactionClicked({productId:product.id,reaction:name}))}
-        >
-            {image} {product.reactions[name]}
-        </button>
-    )
-  };
-  return <div>{userReaction}</div>
+  const userReactions = Object.entries(reactionObj).map(([name, image]) => {
+    return (
+      <button
+        key={name}
+        type="button"
+        className="reaction-button"
+        onClick={() =>
+          dispatch(reactionClicked({ productId: product.id, reaction: name }))
+        }
+      >
+        {image} {product.reactions[name]}
+      </button>
+    );
+  });
+  return <div>{userReactions}</div>;
 };
-export default UserReaction;
+export default UserReactions;
